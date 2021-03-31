aa7db002805277d827bcd4549433e801b20cf521
abhiTagline28 <abhimanyu.tagline@gmail.com>
2021-03-31 12:10:06 +0530

    After Completing filter and reusable also

--- src/data.js
+++ src/data.js
@@ -33,18 +33,81 @@ const data = [
         "type": "C",
         "active": "FALSE"
     },
-    /* {
+    {
         "id": 5,
         "name": "Kamal",
         "city": "pune",
         "category": "three",
         "type": "D",
         "active": "TRUE"
-    }, */
+    },
 
 ]
 
 export default data
+
+
+/*
+
+//************ another data *******
+
+
+const data = [
+    {
+        id: 1,
+        name: "foo",
+        city: "dallas",
+        category: "one",
+        type: "A",
+        state: "Gujarat",
+        active: "FALSE",
+    },
+    {
+        id: 2,
+        name: "bar",
+        city: "dallas",
+        category: "one",
+        type: "B",
+        state: "Bangal",
+        active: "FALSE",
+    },
+    {
+        id: 3,
+        name: "jim",
+        city: "san francisco",
+        category: "one",
+        type: "B",
+        state: "Gujarat",
+        active: "TRUE",
+    },
+    {
+        id: 4,
+        name: "jane",
+        city: "denver",
+        category: "two",
+        type: "C",
+        state: "Maharastra",
+        active: "FALSE",
+    },
+    {
+        id: 5,
+        name: "Kamal",
+        city: "pune",
+        category: "three",
+        type: "D",
+        state: "Gujarat",
+        active: "TRUE",
+    },
+];
+
+export default data; */
+
+
+
+
+
+
+
 
 
 /*  const uniqueVal = (datas) => {
